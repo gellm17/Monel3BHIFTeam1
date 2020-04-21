2ee6a7c02110efd3f156fa1683756be027cbd621
Clemens <clemens.burgmann@gmx.at>
2020-04-21 12:07:18 +0200

    Person and Client
    getters and fields completed

--- Monel3BHIFTEAM1/src/app/Main.java
+++ Monel3BHIFTEAM1/src/app/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("MonelPro Edited by Clemens");
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("MonelPro");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
