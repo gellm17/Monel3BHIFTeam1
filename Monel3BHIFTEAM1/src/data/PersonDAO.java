41f6366bdb6708e99914b465914725a0c6badd85
gellm <michael.gell@gmx.at>
2020-04-21 11:55:54 +0200

    Update Rename
    Rename Class

--- Monel3BHIFTEAM1/src/data/PersonDAO.java
+++ Monel3BHIFTEAM1/src/data/PersonDAO.java
@@ -1,11 +1,14 @@
 package data;
 
-import model.Client;
-import model.Person;
+
+import model.*;
+
+import java.util.ArrayList;
 
 public class PersonDAO {
     private ArrayList<Client> clients = new ArrayList<Client>();            // ArrayList mit allen Personen
-    //private ArrayList<Employee> employees = new ArrayList<Employee>();      // ArrayList mit allen Company
+    private ArrayList<Employee> employees = new ArrayList<Employee>();      // ArrayList mit allen Company
+    private ArrayList<Sponsor> sponsor = new ArrayList<Sponsor>();      // ArrayList mit allen Company
     private static PersonDAO instance = null;
 
     private PersonDAO() {
@@ -20,13 +23,17 @@ public class PersonDAO {
 
     public boolean addPerson(Person p) {
         boolean success = false;
-        if(j instanceof Client) {
+        if(p instanceof Client) {
             Client c = (Client)p;
             success = clients.add(c);
-        } else if {
-            Employee c = (Company) j;
-            success = companies.add(c);
+        } else if (p instanceof Employee){
+            Employee emp = (Employee) p;
+            success = employees.add(emp);
+        } else if (p instanceof Sponsor){
+            Sponsor sp = (Sponsor) p;
+            success = sponsor.add(sp);
         }
+
         return success;
     }
 }
--- Monel3BHIFTEAM1/src/model/Sponsor.java
+++ Monel3BHIFTEAM1/src/model/Sponsor.java
@@ -1,4 +1,4 @@
 package model;
 
-public class Sponsor {
+public class Sponsor extends Person {
 }
