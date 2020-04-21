8f09da116f8084b1d14208b0bf59475d96963acf
gellm <michael.gell@gmx.at>
2020-04-21 14:20:33 +0200

    Update Scene Size
    Fullscreen

--- Monel3BHIFTEAM1/src/app/Main.java
+++ Monel3BHIFTEAM1/src/app/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setFullScreen(true);
         primaryStage.setTitle("MonelPro");
-        primaryStage.setScene(new Scene(root, 1200, 800));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
