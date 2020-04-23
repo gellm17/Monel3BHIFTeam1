490ce0149197e02d66c0e405a37814e0b9114cd5
gellm <michael.gell@gmx.at>
2020-04-23 13:44:35 +0200

    Update fix
    Komische Zeichen

--- Monel3BHIFTEAM1/src/app/Main.java
+++ Monel3BHIFTEAM1/src/app/Main.java
@@ -10,17 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-<<<<<<< HEAD
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("MonelPro");
-        primaryStage.setScene(new Scene(root, 1300, 800));
-=======
         FXMLLoader fxml = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
         primaryStage.setMaximized(true);
         primaryStage.setTitle("MonelPro");
         BorderPane root = fxml.load();
         Scene scene = new Scene(root);
->>>>>>> d00ab52ad947bf3dd991d32a5b6683941ea5c19e
 
         MainWindow_Controller main = fxml.getController();
         main.setPrimaryStage(primaryStage);
