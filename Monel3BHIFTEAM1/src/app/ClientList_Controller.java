9128e8ecf3458ea07d7a7e078f8884c5b3a85e79
ambroschdavid <61412392+ambroschdavid@users.noreply.github.com>
2020-04-30 12:39:52 +0200

    Split MainWindow into Seperate FXMLs
    Ambrosch

--- Monel3BHIFTEAM1/src/app/ClientList_Controller.java
+++ Monel3BHIFTEAM1/src/app/ClientList_Controller.java
@@ -1,4 +1,137 @@
 package app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class ClientList_Controller {
+
+    @FXML
+    private Button btnInfo;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private ImageView imageLogo;
+
+    @FXML
+    private Label lbTitle;
+
+    @FXML
+    private TableView<?> tableClients;
+
+    @FXML
+    private TextField tfSearchClients;
+
+    @FXML
+    private Button btnSearchClients;
+
+    @FXML
+    private Button btnFilterClients;
+
+    @FXML
+    private Button btnAddClient;
+
+    @FXML
+    private Button btnDeleteClient;
+
+    @FXML
+    private Button btnEditClient;
+
+    @FXML
+    private Button btnNavActivities;
+
+    @FXML
+    private Button btnNavBills;
+
+    @FXML
+    private Button btnNavClients;
+
+    @FXML
+    private Button btnNavEmployees;
+
+    @FXML
+    private Button btnNavSponsors;
+
+    @FXML
+    private Button btnNavPhotos;
+
+    @FXML
+    private Button btnNavBirthdays;
+
+    @FXML
+    void btnAddClient_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteClient_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditClient_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnFilterClients_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnInfo_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavActivities_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavBills_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavBirthdays_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavClients_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavEmployees_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavPhotos_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavSponsors_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchClients_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettings_Clicked(ActionEvent event) {
+
+    }
+
 }
--- Monel3BHIFTEAM1/src/app/EmployeeList_Controller.java
+++ Monel3BHIFTEAM1/src/app/EmployeeList_Controller.java
@@ -1,4 +1,137 @@
 package app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class EmployeeList_Controller {
+
+    @FXML
+    private Button btnInfo;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private ImageView imageLogo;
+
+    @FXML
+    private Label lbTitle;
+
+    @FXML
+    private TableView<?> tableEmployees;
+
+    @FXML
+    private TextField tfSearchEmployees;
+
+    @FXML
+    private Button btnSearchEmployees;
+
+    @FXML
+    private Button btnFilterEmployees;
+
+    @FXML
+    private Button btnAddEmployee;
+
+    @FXML
+    private Button btnDeleteEmployee;
+
+    @FXML
+    private Button btnEditEmployee;
+
+    @FXML
+    private Button btnNavActivities;
+
+    @FXML
+    private Button btnNavBills;
+
+    @FXML
+    private Button btnNavClients;
+
+    @FXML
+    private Button btnNavEmployees;
+
+    @FXML
+    private Button btnNavSponsors;
+
+    @FXML
+    private Button btnNavPhotos;
+
+    @FXML
+    private Button btnNavBirthdays;
+
+    @FXML
+    void btnAddEmployee_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteEmployee_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditEmployee_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnFilterEmployees_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnInfo_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavActivities_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavBills_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavBirthdays_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavClients_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavEmployees_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavPhotos_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavSponsors_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchEmployees_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettings_Clicked(ActionEvent event) {
+
+    }
+
 }
