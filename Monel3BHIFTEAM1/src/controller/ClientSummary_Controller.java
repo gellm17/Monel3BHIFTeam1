c0ad9423a6f1ce483e029eae9f42c5e5a33f9a4d
ambroschdavid <61412392+ambroschdavid@users.noreply.github.com>
2020-05-04 12:23:37 +0200

    Created Client Summary
    Created FXML and controller

---
+++ Monel3BHIFTEAM1/src/controller/ClientSummary_Controller.java
@@ -0,0 +1,102 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ClientSummary_Controller {
+
+    @FXML
+    private ImageView imgClient;
+
+    @FXML
+    private Button btnDeleteClient;
+
+    @FXML
+    private Button btnEditClient;
+
+    @FXML
+    private Label lbNameClient;
+
+    @FXML
+    private Label lbSsnrClient;
+
+    @FXML
+    private Label lbBirthdateClient;
+
+    @FXML
+    private Label lbStreetClient;
+
+    @FXML
+    private Label lbPlaceClient;
+
+    @FXML
+    private Label lbPhoneClient;
+
+    @FXML
+    private Label lbEmailClient;
+
+    @FXML
+    private Label lbIbanClient;
+
+    @FXML
+    private Label lbBicClient;
+
+    @FXML
+    private Label lbNameClient1;
+
+    @FXML
+    private Label lbBirthdateClient1;
+
+    @FXML
+    private Label lbStreetClient1;
+
+    @FXML
+    private Label lbPhoneClient1;
+
+    @FXML
+    private Button btnShowEsv;
+
+    @FXML
+    private Button btnShowContact1;
+
+    @FXML
+    private Button btnShowContact2;
+
+    @FXML
+    private Label lbNameEsv;
+
+    @FXML
+    private Label lbNameContact1;
+
+    @FXML
+    private Label lbNameContact2;
+
+    @FXML
+    void btnDeleteClient_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditClient_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnShowContact1_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnShowContact2_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnShowEsv_Clicked(ActionEvent event) {
+
+    }
+
+}
