ff3ea58e35666ea354e3fde0a6821a077901759e
Guggenberger <64003515+Guggenberger@users.noreply.github.com>
2020-05-04 12:01:33 +0200

    Update Sponsor
    Create Sponsor

--- Monel3BHIFTEAM1/src/model/Sponsor.java
+++ Monel3BHIFTEAM1/src/model/Sponsor.java
@@ -4,7 +4,45 @@ import java.time.LocalDate;
 
 public class Sponsor extends Person {
 
-    public Sponsor(Salutation salutation, String firstName, String lastName, String street, String houseNumber, int zipCode, String place, LocalDate birthDate) {
-        super(salutation, firstName, lastName, street, houseNumber, zipCode, place, birthDate);
+    private StringProperty firmenname;
+    private StringProperty firmenTelNr;
+    private StringProperty firmenEmail;
+
+    public Sponsor(Salutation salutation, String firstName, String lastName) {
+        super(salutation, firstName, lastName );
+
+        firmenname = new SimpleIntegerProperty(this, "firmenname");
+        firmenTelNr = new SimpleIntegerProperty(this, "firmenTelNr");
+        firmenEmail = new SimpleIntegerProperty(this, "firmenEmail");
+    }
+
+    public int getFirmenname() {
+        return firmenname.get();
+    }
+    public StringProperty getFirmennameProperty() {
+        return firmenname;
+    }
+    public void setFirmenname(StringProperty firmenname) {
+        this.firmenname = firmenname;
+    }
+
+    public int getFirmenTalNr() {
+        return firmenTelNr.get();
+    }
+    public StringProperty getFirmenTalNrProperty() {
+        return firmenTelNr;
+    }
+    public void setFirmenTalNr(StringProperty firmenTelNr) {
+        this.firmenTelNr = firmenTelNr;
+    }
+
+    public int getFirmenEmail() {
+        return firmenEmail.get();
+    }
+    public StringProperty getFirmenEmailProperty() {
+        return firmenEmail;
+    }
+    public void setFirmenEmail(StringProperty firmenEmail) {
+        this.firmenEmail = firmenEmail;
     }
 }
