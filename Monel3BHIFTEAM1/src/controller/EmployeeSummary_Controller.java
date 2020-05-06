be2cf4729f5771c8a2ffa125dd32e4894382c8c4
ambroschdavid <61412392+ambroschdavid@users.noreply.github.com>
2020-05-06 12:05:22 +0200

    Added EmployeeSummary - FXML
    - Ambrosch

---
+++ Monel3BHIFTEAM1/src/controller/EmployeeSummary_Controller.java
@@ -0,0 +1,75 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class EmployeeSummary_Controller {
+
+    @FXML
+    private ImageView imgEmployee;
+
+    @FXML
+    private Button btnDeleteEmployee;
+
+    @FXML
+    private Button btnEditClient;
+
+    @FXML
+    private Label lbNameEmployee;
+
+    @FXML
+    private Label lbSsnrEmployee;
+
+    @FXML
+    private Label lbBirthdateEmployee;
+
+    @FXML
+    private Label lbStreetEmployee;
+
+    @FXML
+    private Label lbPlaceEmployee;
+
+    @FXML
+    private Label lbPhoneEmployee;
+
+    @FXML
+    private Label lbEmailEmployee;
+
+    @FXML
+    private Label lbIbanEmployee;
+
+    @FXML
+    private Label lbBicEmployee;
+
+    @FXML
+    private Label lbVolunteeringEmployee;
+
+    @FXML
+    private Label lbOccupationGroupEmployee;
+
+    @FXML
+    private Label lbStreetClient1;
+
+    @FXML
+    private Label lbSalaryLevelEmployee;
+
+    @FXML
+    private Label lbDateSalaryLevelEmployee;
+
+    @FXML
+    private Label lbDateOfEmploymentEmployee;
+
+    @FXML
+    void btnDeleteEmployee_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnEditEmployee_Clicked(ActionEvent event) {
+
+    }
+
+}
