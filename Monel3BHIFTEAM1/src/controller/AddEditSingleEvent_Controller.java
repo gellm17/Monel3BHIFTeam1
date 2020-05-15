383b0912ff8278e00561ccd7b7edda42c1baf56a
guggja <61412245+guggja@users.noreply.github.com>
2020-05-15 14:01:15 +0200

    beheben von github fehlern

--- Monel3BHIFTEAM1/src/controller/AddEditSingleEvent_Controller.java
+++ Monel3BHIFTEAM1/src/controller/AddEditSingleEvent_Controller.java
@@ -1,158 +1,154 @@
 package controller;
 
 import app.SceneLoader;
+import data.EventDAO;
 import data.PersonDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-<<<<<<< HEAD:Monel3BHIFTEAM1/src/controller/AddEditSingleEvent_Controller.java
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-=======
 import javafx.scene.control.*;
->>>>>>> 5a38d2b83b760d6c6bdc362b97871c060e396d42:Monel3BHIFTEAM1/src/controller/AddEditEvent_Controller.java
 import javafx.scene.image.ImageView;
 import javafx.util.converter.LocalDateStringConverter;
 import model.Employee;
 import model.Event;
 
-<<<<<<< HEAD:Monel3BHIFTEAM1/src/controller/AddEditSingleEvent_Controller.java
 public class AddEditSingleEvent_Controller {
-=======
-public class AddEditEvent_Controller extends SceneLoader {
->>>>>>> 5a38d2b83b760d6c6bdc362b97871c060e396d42:Monel3BHIFTEAM1/src/controller/AddEditEvent_Controller.java
 
-    @FXML
-    private Button btnInfo;
+    public class AddEditEvent_Controller extends SceneLoader {
 
-    @FXML
-    private Button btnSettings;
 
-    @FXML
-    private ImageView imageLogo;
+        @FXML
+        private Button btnInfo;
 
-    @FXML
-    private Label lbTitle;
+        @FXML
+        private Button btnSettings;
 
-    @FXML
-    private DatePicker dpDateEvent;
+        @FXML
+        private ImageView imageLogo;
 
-    @FXML
-    private TextField tfNameEvent;
+        @FXML
+        private Label lbTitle;
 
-    @FXML
-    private ComboBox<?> comboClientEvent;
+        @FXML
+        private DatePicker dpDateEvent;
 
-    @FXML
-    private ComboBox<?> comboEmployeeEvent;
+        @FXML
+        private TextField tfNameEvent;
 
-    @FXML
-    private TextField tfStartEvent;
+        @FXML
+        private ComboBox<?> comboClientEvent;
 
-    @FXML
-    private TextField tfEndEvent;
+        @FXML
+        private ComboBox<?> comboEmployeeEvent;
 
-    @FXML
-    private TextField tfHourlyRateEvent;
+        @FXML
+        private TextField tfStartEvent;
 
-    @FXML
-    private TextField tfRideCostsEvent;
+        @FXML
+        private TextField tfEndEvent;
 
-    @FXML
-    private Label lbMessage;
+        @FXML
+        private TextField tfHourlyRateEvent;
 
-    @FXML
-    private Button btnCancelEvent;
+        @FXML
+        private TextField tfRideCostsEvent;
 
-    @FXML
-    private Button btnOkEvent;
+        @FXML
+        private Label lbMessage;
 
-    private Event editableEvent = null;
-    private int errorCounter = 0;
+        @FXML
+        private Button btnCancelEvent;
 
-    public Event getEditableEvent() {
-        return editableEvent;
-    }
+        @FXML
+        private Button btnOkEvent;
 
-    public void setEditableEvent(Event editableEvent) {
-        this.editableEvent = editableEvent;
-        if (editableEvent != null){
-            dpDateEvent.setValue(editableEvent.getDate());
-            tfNameEvent.setText(editableEvent.getName());
-        }
-    }
+        private Event editableEvent = null;
+        private int errorCounter = 0;
 
-    @FXML
-    void btnCancelEvent_Clicked(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnInfo_Clicked(ActionEvent event) {
-
-    }
-
-    @FXML
-    void btnOkEvent_Clicked(ActionEvent event) {
-        Event eventToAdd = new Event();
-        errorCounter = 0;
-
-        try {
-            LocalDateStringConverter ldsc = new LocalDateStringConverter();
-            eventToAdd.setDate(ldsc.fromString(dpDateEvent.getEditor().getText()));
-            dpDateEvent.setStyle(null);
-        } catch (Exception e) {
-            dpDateEvent.setStyle("-FX-Border-Color: red");
-            errorCounter++;
-            lbMessage.setText("Es sind " + errorCounter + " Fehler aufgetreten!");
+        public Event getEditableEvent() {
+            return editableEvent;
         }
 
-        if (!tfCheck(tfNameEvent, "^.+$")) {
-            eventToAdd.setName(tfNameEvent.getText());
-        }
-
-        if(errorCounter == 0 && EventDAO.getInstance().addEvent(eventToAdd)) {
-            if (editableEvent != null) { PersonDAO.getInstance().deleteEvent(editableEvent);
+        public void setEditableEvent(Event editableEvent) {
+            this.editableEvent = editableEvent;
+            if (editableEvent != null) {
+                dpDateEvent.setValue(editableEvent.getDate());
+                tfNameEvent.setText(editableEvent.getName());
             }
-            super.showScene("EmployeeList");
+        }
+
+        @FXML
+        void btnCancelEvent_Clicked(ActionEvent event) {
+
+        }
+
+        @FXML
+        void btnInfo_Clicked(ActionEvent event) {
+
+        }
+
+        @FXML
+        void btnOkEvent_Clicked(ActionEvent event) {
+            Event eventToAdd = new Event();
+            errorCounter = 0;
+
+            try {
+                LocalDateStringConverter ldsc = new LocalDateStringConverter();
+                eventToAdd.setDate(ldsc.fromString(dpDateEvent.getEditor().getText()));
+                dpDateEvent.setStyle(null);
+            } catch (Exception e) {
+                dpDateEvent.setStyle("-FX-Border-Color: red");
+                errorCounter++;
+                lbMessage.setText("Es sind " + errorCounter + " Fehler aufgetreten!");
+            }
+
+            if (!tfCheck(tfNameEvent, "^.+$")) {
+                eventToAdd.setName(tfNameEvent.getText());
+            }
+
+            if (errorCounter == 0 && EventDAO.getInstance().addEvent(eventToAdd)) {
+                if (editableEvent != null) {
+                    EventDAO.getInstance().deleteEvent(editableEvent);
+                }
+                super.showScene("EmployeeList");
+            }
+        }
+
+        @FXML
+        void btnSettings_Clicked(ActionEvent event) {
+
+        }
+
+        private boolean tfCheck(TextField tf, String regex) {
+            boolean error = true;
+            if (!tf.getText().matches(regex)) {
+                error = true;
+                errorCounter++;
+                tf.setStyle("-FX-Border-Color: red");
+                lbMessage.setText("Es sind " + errorCounter + " Fehler aufgetreten!");
+            } else {
+                error = false;
+                tf.setStyle(null);
+            }
+            return error;
+        }
+
+        private boolean taCheck(TextArea ta, String regex) {
+            boolean error = true;
+            if (!ta.getText().matches(regex)) {
+                error = true;
+                errorCounter++;
+                ta.setStyle("-FX-Border-Color: red");
+                lbMessage.setText("Es sind " + errorCounter + " Fehler aufgetreten!");
+            } else {
+                error = false;
+                ta.setStyle(null);
+            }
+            return error;
         }
     }
-
-    @FXML
-    void btnSettings_Clicked(ActionEvent event) {
-
-    }
-
-    private boolean tfCheck(TextField tf, String regex){
-        boolean error = true;
-        if (!tf.getText().matches(regex)) {
-            error = true;
-            errorCounter++;
-            tf.setStyle("-FX-Border-Color: red");
-            lbMessage.setText("Es sind " + errorCounter + " Fehler aufgetreten!");
-        } else {
-            error = false;
-            tf.setStyle(null);
-        }
-        return error;
-    }
-
-    private boolean taCheck(TextArea ta, String regex){
-        boolean error = true;
-        if (!ta.getText().matches(regex)) {
-            error = true;
-            errorCounter++;
-            ta.setStyle("-FX-Border-Color: red");
-            lbMessage.setText("Es sind " + errorCounter + " Fehler aufgetreten!");
-        } else {
-            error = false;
-            ta.setStyle(null);
-        }
-        return error;
-    }
-
-
-
 }
