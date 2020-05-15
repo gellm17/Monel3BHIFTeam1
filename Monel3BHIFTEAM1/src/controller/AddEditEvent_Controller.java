3cac0b718103c4c1503953985c6fe4baabf38b51
ambroschdavid <61412392+ambroschdavid@users.noreply.github.com>
2020-05-15 09:23:35 +0200

    Added Event and Protocol

---
+++ Monel3BHIFTEAM1/src/controller/AddEditEvent_Controller.java
@@ -0,0 +1,67 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class AddEditEvent_Controller {
+
+    @FXML
+    private Button btnInfo;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private ImageView imageLogo;
+
+    @FXML
+    private Label lbTitle;
+
+    @FXML
+    private DatePicker dpDateEvent;
+
+    @FXML
+    private TextField tfNameEvent;
+
+    @FXML
+    private TextArea taDescriptionEvent;
+
+    @FXML
+    private TextField tfRideCostsEvent;
+
+    @FXML
+    private Label lbMessage;
+
+    @FXML
+    private Button btnCancelEvent;
+
+    @FXML
+    private Button btnOkEvent;
+
+    @FXML
+    void btnCancelEvent_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnInfo_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnOkEvent_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettings_Clicked(ActionEvent event) {
+
+    }
+
+}
