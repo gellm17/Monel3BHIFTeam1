778a614e1c95065f17b8f2ff60c02034dca31ec5
ambroschdavid <61412392+ambroschdavid@users.noreply.github.com>
2020-05-28 10:44:55 +0200

    Added Billlist per Client

--- Monel3BHIFTEAM1/src/controller/BillList_Controller.java
+++ Monel3BHIFTEAM1/src/controller/BillList_Controller.java
@@ -1,4 +1,111 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class BillList_Controller {
+
+    @FXML
+    private Button btnInfo;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private ImageView imageLogo;
+
+    @FXML
+    private Label lbTitle;
+
+    @FXML
+    private TableView<?> tableProtocols;
+
+    @FXML
+    private TextField tfSearchClient;
+
+    @FXML
+    private TextField tfSearchEmployee;
+
+    @FXML
+    private TableView<?> tableEvents;
+
+    @FXML
+    private Button btnViewBills;
+
+    @FXML
+    private Button btnNavActivities;
+
+    @FXML
+    private Button btnNavBills;
+
+    @FXML
+    private Button btnNavClients;
+
+    @FXML
+    private Button btnNavEmployees;
+
+    @FXML
+    private Button btnNavSponsors;
+
+    @FXML
+    private Button btnNavPhotos;
+
+    @FXML
+    private Button btnNavBirthdays;
+
+    @FXML
+    void btnInfo_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavActivities_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavBills_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavBirthdays_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavClients_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavEmployees_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavPhotos_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNavSponsors_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettings_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnViewBills_Clicked(ActionEvent event) {
+
+    }
+
 }
---
+++ Monel3BHIFTEAM1/src/controller/BillsPerClientList_Controller.java
@@ -0,0 +1,63 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+public class BillsPerClientList_Controller {
+
+    @FXML
+    private Button btnPrevMonth;
+
+    @FXML
+    private Label lbYearMonth;
+
+    @FXML
+    private Button btnNextMonth;
+
+    @FXML
+    private TableView<?> tableProtocols;
+
+    @FXML
+    private Button btnPrintBill;
+
+    @FXML
+    private Button btnInfo;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Label lbTitle;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void btnInfo_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnNextMonth_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPrevMonth_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPrintBill_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettings_Clicked(ActionEvent event) {
+
+    }
+
+}
