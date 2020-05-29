a9457087e656fa1d85ddf75d398daef7b0a3ed3a
ambroschdavid <61412392+ambroschdavid@users.noreply.github.com>
2020-05-29 13:39:55 +0200

    added Print Preview

---
+++ Monel3BHIFTEAM1/src/controller/ViewBill_Controller.java
@@ -0,0 +1,56 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ViewBill_Controller {
+
+    @FXML
+    private Label lbClientOnBill;
+
+    @FXML
+    private Label lbMonthOnBill;
+
+    @FXML
+    private Label lbSingleEvents;
+
+    @FXML
+    private Label lbGroupEvents;
+
+    @FXML
+    private Label lbRideCosts;
+
+    @FXML
+    private Button btnPrint;
+
+    @FXML
+    private Label lbClient;
+
+    @FXML
+    private Label lbMonth;
+
+    @FXML
+    private Button btnInfo;
+
+    @FXML
+    private Button btnSettings;
+
+    @FXML
+    private Label lbTitle;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    void btnInfo_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSettings_Clicked(ActionEvent event) {
+
+    }
+
+}
